25b22b7851e2f2f5637315a9245ddc6569fd30e2
Eric Kascic <eric.kascic@stelligent.com>
2016-07-20 16:02:10 -0400

    wip

--- pipeline/run-static-analysis.sh
+++ pipeline/run-static-analysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 bundle --gemfile pipeline/Gemfile \
-       exec cfn_nag --input-json-path cfn/template-logging.json
+       exec 'cfn_nag --input-json-path cfn/template-logging.json'
