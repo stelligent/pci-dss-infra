7917718763c45d1c800b0dac816e6b9945d1b3b3
Eric Kascic <eric.kascic@stelligent.com>
2016-07-20 17:03:12 -0400

    wip

--- pipeline/create-infrastructure.sh
+++ pipeline/create-infrastructure.sh
@@ -4,5 +4,7 @@ mkdir /var/tmp/pci-dss-infra || true
 
 bundle install --path /var/tmp/pci-dss-infra
 
+export AWS_REGION=us-west-2
+
 bundle exec cfn_converge --path-to-stack cfn/template-logging.json \
                          --stack-name something-cool
