77e226c927accf1cb46748983a963c2ce93530aa
Eric Kascic <eric.kascic@stelligent.com>
2016-07-20 16:09:42 -0400

    wip

--- pipeline/run-static-analysis.sh
+++ pipeline/run-static-analysis.sh
@@ -5,4 +5,5 @@ mkdir /var/tmp/pci-dss-infra || true
 bundle install --gemfile pipeline/Gemfile \
                --path /var/tmp/pci-dss-infra
 
-bundle exec cfn_nag --input-json-path cfn/template-logging.json
+bundle exec --gemfile pipeline/Gemfile \
+       cfn_nag --input-json-path cfn/template-logging.json
