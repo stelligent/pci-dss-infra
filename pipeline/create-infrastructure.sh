d6826ae24d03b037262c2e3d92f05380efbc8e29
Eric Kascic <eric.kascic@stelligent.com>
2016-07-21 13:17:43 -0400

    wip

--- pipeline/create-infrastructure.sh
+++ pipeline/create-infrastructure.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
+env=ci
+
 mkdir /var/tmp/pci-dss-infra || true
 
 bundle install --path /var/tmp/pci-dss-infra
@@ -7,4 +9,5 @@ bundle install --path /var/tmp/pci-dss-infra
 export AWS_REGION=us-west-2
 
 bundle exec cfn_converge --path-to-stack cfn/template-logging.json \
-                         --stack-name something-cool
+                         --stack-name something-cool \
+                         --path-to-yaml env/${env}/logging.yml
